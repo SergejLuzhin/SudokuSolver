cc5712560cae7153979a54ddb12ad0e4d7256377
Sergej <luzhins764@gmail.com>
2024-11-11 18:59:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    static int[][] initial_field = {
+            {4, 0, 3, 0, 0, 2, 0, 0, 0},
+            {5, 0, 0, 0, 6, 0, 1, 2, 0},
+            {9, 0, 0, 0, 0, 0, 0, 0, 4},
+            {0, 0, 8, 0, 7, 0, 0, 0, 0},
+            {0, 0, 0, 2, 0, 3, 0, 0, 8},
+            {0, 3, 6, 0, 0, 0, 7, 0, 0},
+            {0, 7, 0, 9, 2, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 5, 0, 9, 6},
+            {0, 0, 0, 8, 0, 4, 5, 0, 0}};
+
+    public static void main(String[] args) {
+        SudokuSolver sudokuSolver = new SudokuSolver();
+
+        int[][] field_sudokued = sudokuSolver.sudoku(initial_field);
+
+        for (int i = 0; i < 9; i++) {
+            System.out.print("[");
+            for (int j = 0; j < 9; j++) {
+                System.out.print(field_sudokued[i][j] + ", ");
+            }
+            System.out.print("]\n");
+        }
+
+    }
+}
